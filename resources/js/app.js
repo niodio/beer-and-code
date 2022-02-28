17ab8b5b69630b73f8dd255aeae37f9362354307
niodio <adelson.inacio@sweetmedia.tech>
2022-02-28 17:20:59 +0000

    isntalando vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,14 +3,24 @@ require("./bootstrap");
 import { createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/inertia-vue3";
 import { InertiaProgress } from "@inertiajs/progress";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
 InertiaProgress.init();
+
+const vuetify = createVuetify({
+    components,
+    directives,
+});
+// Replaces new Vuetify(...)
 
 createInertiaApp({
     resolve: (name) => require(`./Pages/${name}`),
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(vuetify)
             .mount(el);
     },
 });
