ac9138324daa155062c290ed7c1925200fa58338
niodio <adelson.inacio@sweetmedia.tech>
2022-02-28 16:43:24 +0000

    inertia funcionando

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>{{ dashboard }}</h1>
+</template>
+
+<script>
+export default {
+    el: "#app",
+    name: "Dashboard",
+    props: ["title"],
+
+    data() {
+        return {
+            dashboard: "Dashboard",
+        };
+    },
+
+    mounted() {
+        this.dashboard = this.title;
+
+        alert(this.dashboard);
+    },
+};
+</script>
+
+<style></style>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+@extends('app')
+
+@section('content')
+    <h1>Bem vindo ao Beer and Code</h1>
+@endsection
